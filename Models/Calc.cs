3adda7046a0efd28efce85d7fd1cb4c5f32a86bb
douglascarrie <douglascarrie@hotmail.co.uk>
2023-03-17 08:44:57 +0000

    First commit

---
+++ Models/Calc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Calc
+    {
+        public float value1 { get; set; }
+        public float value2 { get; set; }
+        public float result { get; set; }
+        public string calculate { get; set; }
+
+    }
+}
